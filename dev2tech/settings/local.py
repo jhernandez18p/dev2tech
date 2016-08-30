be2c7a878667d86e1e04eb7fe1bd7a9520e1853f
Josmer Hernandez <jhernandez.18p@gmail.com>
2016-08-30 10:26:30 -0500

    DEV2TECH

--- dev2tech/settings/local.py
+++ dev2tech/settings/local.py
@@ -7,10 +7,10 @@ STATIC_ROOT = os.path.abspath(os.path.join(os.path.join(BASE_DIR,os.pardir),'sta
 DATABASES ={
             'default':{
                 'ENGINE': 'django.db.backends.postgresql',
-                'NAME': config('INTRA_DB_NAME'),
-                'USER': config('INTRA_DB_USER'),
-                'PASSWORD': config('INTRA_DB_PASSWORD'),
-                'HOST': config('INTRA_DB_HOST'),
-                'PORT': config('INTRA_DB_PORT',cast=int),
+                'NAME': config('DEV2TECH_DB_NAME'),
+                'USER': config('DEV2TECH_DB_USER'),
+                'PASSWORD': config('DEV2TECH_DB_PASSWORD'),
+                'HOST': config('DEV2TECH_DB_HOST'),
+                'PORT': config('DEV2TECH_DB_PORT',cast=int),
                 }
             }
--- dev2tech/settings/production.py
+++ dev2tech/settings/production.py
@@ -1,9 +1,9 @@
 from dev2tech.settings.base import *
 
 
-SECRET_KEY = config('INTRA_SECRET_KEY')
+SECRET_KEY = config('DEV2TECH_SECRET_KEY')
 
-DEBUG = config('INTRA_DEBUG',default=False, cast=bool)
+DEBUG = config('DEV2TECH_DEBUG',default=False, cast=bool)
 
 ALLOWED_HOSTS = ['intra.phoenixworldtrade.com']
 
@@ -12,10 +12,10 @@ ALLOWED_HOSTS = ['intra.phoenixworldtrade.com']
 DATABASES ={
             'default':{
                 'ENGINE': 'django.db.backends.postgresql',
-                'NAME': config('INTRA_DB_NAME'),
-                'USER': config('INTRA_DB_USER'),
-                'PASSWORD': config('INTRA_DB_PASSWORD'),
-                'HOST': config('INTRA_DB_HOST'),
-                'PORT': config('INTRA_DB_PORT',cast=int),
+                'NAME': config('DEV2TECH_DB_NAME'),
+                'USER': config('DEV2TECH_DB_USER'),
+                'PASSWORD': config('DEV2TECH_DB_PASSWORD'),
+                'HOST': config('DEV2TECH_DB_HOST'),
+                'PORT': config('DEV2TECH_DB_PORT',cast=int),
                 }
             }
