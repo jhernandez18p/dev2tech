117704060c2a188f754ca007a42f43bb85592d1c
Josmer Hernández <jhernandez.18p@gmail.com>
2016-12-28 13:16:47 -0500

    Some Updates

--- staticfiles/base/js/app.js
+++ staticfiles/base/js/app.js
@@ -44,9 +44,7 @@ $(document).ready(function($)
 
     $(".content-markdown").each(function () {
         var content = $(this).text();
-        console.log(content);
         var markedContent = marked(content);
-        console.log(markedContent);
         $(this).html(markedContent)
     })
 });
