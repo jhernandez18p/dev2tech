9b28c0545767ed537e3ec8d978e4e45b34e8b4d3
Josmer Hernández <jhernandez.18p@gmail.com>
2017-03-12 13:30:32 -0500

    git pull

--- settings/settings/custom_context_processors.py
+++ settings/settings/custom_context_processors.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import random
-from local_apps.services.models import Services
+# from local_apps.services.models import Services
 
 def menu(request):
-    top_services = list(Services.objects.all())
-    random.shuffle(top_services)
+    # top_services = list(Services.objects.all())
+    # random.shuffle(top_services)
     context = {
         'menu_es':["inicio","conocenos","¿qué hacemos?","¡lo hemos hecho!","contacto",],
         'menu_en':["home","who we are","what we do?","what we´ve done?","contact",],
-        'Top_5_Services':top_services,
+        # 'Top_5_Services':top_services,
     }
     return context
--- settings/settings/production.py
+++ settings/settings/production.py
@@ -8,12 +8,12 @@ WSGI_APPLICATION = 'settings.wsgi_prod.application'
 ALLOWED_HOSTS = ['dev2tech.xyz','www.dev2tech.xyz']
 
 DATABASES ={
-            'default':{
-                'ENGINE': 'django.db.backends.postgresql',
-                'NAME': config('DEV2TECH_DB_NAME'),
-                'USER': config('DEV2TECH_DB_USER'),
-                'PASSWORD': config('DEV2TECH_DB_PASSWORD'),
-                'HOST': config('DEV2TECH_DB_HOST'),
-                'PORT': config('DEV2TECH_DB_PORT',cast=int),
-                }
-            }
+	'default':{
+	    'ENGINE': 'django.db.backends.postgresql',
+	    'NAME': config('DEV2TECH_DB_NAME'),
+	    'USER': config('DEV2TECH_DB_USER'),
+	    'PASSWORD': config('DEV2TECH_DB_PASSWORD'),
+	    'HOST': config('DEV2TECH_DB_HOST'),
+	    'PORT': config('DEV2TECH_DB_PORT',cast=int),
+    }
+}
